be2f5b0f84b7a3ed3ca8e83daaad94964524d582
Moto Chowdhury <motochowdhury@gmail.com>
2023-03-22 20:42:37 +0600

    setup home section with routing

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+// External Import
+import {COLORS, incons, images, SIZES} from '../constants';
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>
-                Hello Everyone
-            </Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen options={{
+                headerStyle: {
+                    backgroundColor: COLORS.lightWhite
+                }
+            }} />
+        </SafeAreaView>
     )
 }
 
