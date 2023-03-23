8dfafc52a62a15eb4c1f73b53946aec31ac64e9c
Moto Chowdhury <motochowdhury@gmail.com>
2023-03-23 10:23:38 +0600

    jobs categories added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -7,6 +7,8 @@ import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
   const router = useRouter()
+  const jobTypes = ["Full-time", "Part-time", "Contractor", "Remote"]
+  const [activeJobType, setActiveJobType] = useState("Full-time")
   return (
     <View>
       <View style={styles.container}>
@@ -25,6 +27,16 @@ const Welcome = () => {
         <TouchableOpacity style={styles.searchBtn} onPress={()=>{}} >
             <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
            </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList data={jobTypes} renderItem={({item})=>(
+          <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={()=>{
+            setActiveJobType(item);
+            router.push(`/search/${item}`)
+          }}>
+            <Text style={styles.tabText(activeJobType,item)} >{item}</Text>
+          </TouchableOpacity>
+         )} keyExtractor={item => item} contentContainerStyle={{columnGap: SIZES.small}} horizontal />
       </View>
     </View>
   )
