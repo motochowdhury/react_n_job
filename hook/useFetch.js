dbc227ed636f5ac6e3d6d6f645008fcf4c049e7a
Moto Chowdhury <motochowdhury@gmail.com>
2023-03-25 17:11:28 +0600

    fixed all the issue at data fatching

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,6 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
-import {RAPID_API_KEY} from "@env";
 
-const rapidApiKey= RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
@@ -13,7 +11,7 @@ const useFetch = (endpoint, query) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': rapidApiKey,
+            'X-RapidAPI-Key': '2b457d2355msh1dce226a8c97952p19a6a1jsn8c4456361385',
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
           },
         params: {...query},
@@ -46,3 +44,5 @@ const useFetch = (endpoint, query) => {
 
     return{data,isLoading,error,refetch};
 }
+
+export default useFetch;
