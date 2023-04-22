e254eca66327a11bd21cef37a0b0caba5f69f046
motochowdhury <motochowdhury@gmail.com>
2023-04-22 23:56:45 +0600

    user name changed

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = ({searchTerm, setSearchTerm, handleClick}) => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Moto</Text>
+        <Text style={styles.userName}>Hello, This is Moto</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
